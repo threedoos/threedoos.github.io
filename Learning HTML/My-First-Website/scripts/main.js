41671b720d99f27b9d1ae48a1946de8f324e073b
threedoos <darius8lee2005@gmail.com>
2025-04-27 17:07:05 +0800

    Add files via upload

---
+++ Learning HTML/My-First-Website/scripts/main.js
@@ -0,0 +1,36 @@
+const myImage = document.querySelector("img");
+
+/* This is how you add comments */
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/Spiderman-photo-1.avif") {
+    myImage.setAttribute("src", "images/superman.webp");
+  } else {
+    myImage.setAttribute("src", "images/Spiderman-photo-1.avif");
+  }
+});
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Who is your favourite superhero, ${myName}`;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Who is your favourite superhero, ${storedName}`;
+}
+
+myButton.addEventListener("click", () => {
+  setUserName();
+});
